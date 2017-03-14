8041d638062f352c158529c8f4bf1d3e02fb6053
Mrigesh Madaan <f2015756@pilani.bits-pilani.ac.in>
2017-03-14 13:25:18 +0530

    first draft complete

--- js/master.js
+++ js/master.js
@@ -1,13 +1,23 @@
-$(window).on('load', function() {
+$(document).ready(function() {
     var text1 = "Welcome to Bits Pilani ";
     i = 0;
     var scroll_start = 0;
-    var offset = $("#navmain").offset();
-   
+    var offset = 200;
+    // $('html, body').animate({
+    //     scrollTop: $("#navmain").offset().top
+    // }, 'slow');
 
     function addText(text2, i) {
         $('#text-value').text(text2.substring(0, i + 1));
     }
+    $(window).scroll(function() {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset) {
+            $('.arrow').css('display', 'none');
+        } else {
+            $('.arrow').css('display', 'block');
+        }
+    });
     setTimeout(function() {
         setInterval(function() {
             i++;
@@ -31,5 +41,39 @@ $(window).on('load', function() {
                 }, 1000);
             }
         }, 100);
-    }, 100);
+    }, 1000);
+    $('.page_1').click(
+        function() {
+            $('html, body').animate({
+                scrollTop: $("#about").offset().top
+            }, 'slow');
+        });
+
+    $('.page_2').click(
+        function() {
+            $('html, body').animate({
+                scrollTop: $("#speakers").offset().top
+            }, 'slow');
+        });
+
+    $('.page_3').click(
+        function() {
+            $('html, body').animate({
+                scrollTop: $("#events").offset().top
+            }, 'slow');
+        });
+
+    $('.page_4').click(
+        function() {
+            $('html, body').animate({
+                scrollTop: $("#map").offset().top
+            }, 'slow');
+        });
+
+    $('.page_5').click(
+        function() {
+            $('html, body').animate({
+                scrollTop: $("#team").offset().top
+            }, 'slow');
+        });
 });
